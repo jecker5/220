b5c21d107f27095cee02a3723ffddfad38956124
Joseph Decker <Deckerjd@g.cofc.edu>
2022-01-20 09:47:59 -0500

    Completed HW1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -7,8 +7,10 @@ I certify that this assignment is entirely my own work.
 
 
 def calc_rec_area():
-    pass
-
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 def calc_volume():
     length = eval(input("Enter the length"))

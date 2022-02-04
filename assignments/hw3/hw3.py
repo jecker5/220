d7a04e4dad1303e8bed0005bd37619de007f378b
Joseph Decker <Deckerjd@g.cofc.edu>
2022-02-04 14:15:45 -0500

    Completed hw3

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,34 +1,57 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Name: Joseph Decker
+hw3.py
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
+import math
 
 
 def average():
-    pass
+    acc = 0
+    total = 0
+    inputs = (eval(input("How many grades will you enter?")))
+    for _ in range(inputs):
+        grade = eval(input("Enter grade: "))
+        total = grade + total
+        acc = acc + 1
+    final_grade = total / acc
+    print(final_grade)
 
 
 def tip_jar():
-    pass
+    total = 0
+    for i in range(5):
+        donation = eval(input("How much would you like to donate?"))
+        total = total + donation
+    print(total)
 
 
 def newton():
-    pass
+    number_1 = eval(input("What number would you like to square root?"))
+    number_2 = eval(input("How many times should we improve the approximation?"))
+    approx = number_1
+    for _ in range(number_2):
+        approx = (approx + (number_1/approx)) / 2
+    print(approx)
 
 
 def sequence():
-    pass
+    number = eval(input("Enter amount of numbers"))
+    for x in range(0, number):
+        y = 1 + (x // 2 * 2)
+        print(y)
 
 
 def pi():
-    pass
+    terms = eval(input("How many terms in the series?"))
+    acc = 1
+    for y in range(terms):
+        num = 2 + (y // 2*2)
+        denom = 1 + ((y + 1) // 2*2)
+        acc = acc * (num / denom)
+    print(acc * 2)
 
 
 if __name__ == '__main__':

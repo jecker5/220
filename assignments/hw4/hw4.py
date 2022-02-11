54970237fdbfadb8daffc21c3b804f49ee0800ea
Joseph Decker <Deckerjd@g.cofc.edu>
2022-02-11 15:06:03 -0500

    Completed hw4

--- assignments/hw4/hw4.py
+++ assignments/hw4/hw4.py
@@ -1,15 +1,13 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Joseph Decker
+hw4.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Create functions using the graphics library
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
-
+import math
 from graphics import *
 
 
@@ -24,40 +22,91 @@ def squares():
 
     # create a space to instruct user
     inst_pt = Point(width / 2, height - 10)
-    instructions = Text(inst_pt, "Click to move circle")
+    instructions = Text(inst_pt, "Click to move square")
     instructions.draw(win)
 
     # builds a circle
-    shape = Circle(Point(50, 50), 20)
+    shape = Rectangle(Point(50, 50), Point(20, 20))
     shape.setOutline("red")
     shape.setFill("red")
     shape.draw(win)
 
     # allows the user to click multiple times to move the circle
-    for i in range(num_clicks):
-        click = win.getMouse()
-        center = shape.getCenter()  # center of circle
+    for _ in range(num_clicks):
+        p = win.getMouse()
+        shape = Rectangle(Point(p.getX() - 20, p.getY() - 20), Point(p.getX() + 10, p.getY() + 10))
+        shape.setOutline("red")
+        shape.setFill("red")
+        shape.draw(win)
 
-        # move amount is distance from center of circle to the
-        # point where the user clicked
-        change_x = click.getX() - center.getX()
-        change_y = click.getY() - center.getY()
-        shape.move(change_x, change_y)
+    instructions.undraw()
+    instructions.setText("Click to close")
+    instructions.draw(win)
 
     win.getMouse()
     win.close()
 
 
 def rectangle():
-    pass
+    win = GraphWin("Rectangle", 400, 400)
+    point1 = win.getMouse()
+    point2 = win.getMouse()
+    rect = Rectangle(point1, point2)
+    rect.draw(win)
+    length = abs(point2.getX() - point1.getX())
+    width = abs(point2.getY() - point1.getY())
+    area = length * width
+    area_txt = Text(Point(200, 350), "The area = " + str(area))
+    perimeter = 2 * (length + width)
+    perimeter_txt = Text(Point(200, 360), "The perimeter = " + str(perimeter))
+    area_txt.draw(win)
+    perimeter_txt.draw(win)
+
+    win.getMouse()
+    win.close()
 
 
 def circle():
-    pass
+    win = GraphWin("Circle", 400, 400)
+    click_1 = win.getMouse()
+    click_2 = win.getMouse()
+    x_1 = click_1.getX()
+    y_1 = click_1.getY()
+    x_2 = click_2.getX()
+    y_2 = click_2.getY()
+    radius = ((x_2-x_1)**2 + (y_2-y_1)**2)**0.5
+    c = Circle(click_1, radius)
+    c.draw(win)
+    radius_text = Text(Point(200, 340), "The radius is: " + str(radius))
+    radius_text.draw(win)
+
+    win.getMouse()
+    win.close()
 
 
 def pi2():
-    pass
+    terms = eval(input("Enter the number of terms to sum: "))
+    acc = 0
+    for i in range(terms):
+        num = 4 * ((-1)**i)
+        denom = 1 + (2 * i)
+        acc += num/denom
+
+    print("Pi approximation: " + str(acc))
+    accuracy = math.pi - acc
+    print("accuracy: " + str(accuracy))
+
+
+
+def main():
+
+    # squares()
+    # rectangle()
+    # circle()
+    pi2()
+
+
+main()
 
 
 if __name__ == '__main__':

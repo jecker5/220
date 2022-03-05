6985db78aeaa278b4d3fae97f306ea82a3b30d1b
Joseph Decker <Deckerjd@g.cofc.edu>
2022-03-04 23:55:00 -0500

    Completed hw7

--- assignments/hw7/hw7.py
+++ assignments/hw7/hw7.py
@@ -1,38 +1,66 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Name: Joseph Decker
+hw7.py
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
+
+from encryption import encode
 
 
 def number_words(in_file_name, out_file_name):
-    pass
+    infile = open(in_file_name, "r")
+    outfile = open(out_file_name, "w+")
+    acc = 1
+    for line in infile:
+        words = line.split()
+        for word in words:
+            outfile.write(str(acc))
+            acc += 1
+            outfile.write(" ")
+            outfile.write(word)
+            outfile.write("\n")
 
 
 def hourly_wages(in_file_name, out_file_name):
-    pass
+    infile = open(in_file_name, "r")
+    outfile = open(out_file_name, "w+")
+    for line in infile:
+        parts = line.split()
+        wage = float(parts[2])
+        wage = wage + 1.65
+        wage = wage * int(parts[3])
+        outfile.write(parts[0])
+        outfile.write(" ")
+        outfile.write(parts[1])
+        outfile.write(" ")
+        outfile.write("{:.2f}".format(wage))
+        outfile.write("\n")
 
 
 def calc_check_sum(isbn):
-    pass
+    acc = 0
+    isbn = isbn.replace('-', '')
+    for i in range(10):
+        pos = 10 - i
+        acc += (pos * int(isbn[i]))
+    return acc
 
 
 def send_message(file_name, friend_name):
-    pass
-
-
-def encode():
-    pass
+    infile = open(file_name, "r")
+    outfile = open(friend_name + ".txt", "w+")
+    for line in infile:
+        outfile.write(line)
 
 
 def send_safe_message(file_name, friend_name, key):
-    pass
+    infile = open(file_name, "r")
+    outfile = open(friend_name + ".txt", "w+")
+    for line in infile:
+        outfile.write(encode(line, key))
+        outfile.write("\n")
 
 
 def send_uncrackable_message(file_name, friend_name, pad_file_name):
